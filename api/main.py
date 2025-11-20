115d80892c427bed1ff1ec8bfca2c8f674a29664
Selina Zhang <selinaz@crc-dot1x-nat-10-239-110-5.bu.edu>
2025-11-20 16:18:13 -0500

    finished bug

--- api/main.py
+++ api/main.py
@@ -4,20 +4,20 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-
 # 让 Python 找到 app/
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+APP_DIR = os.path.join(BASE_DIR, "..", "app")
+sys.path.append(APP_DIR)
 
 from app.core import router as core_router
 
 app = FastAPI()
 
-# 自动根据 main.py 的位置挂载 static
-BASE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "app")
+# 如果你还没有静态文件，暂时不挂载
+# app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
-#app.mount("/static", StaticFiles(directory="app/static"), name="static")
+# 模板目录（必须存在）
 templates = Jinja2Templates(directory="app/templates")
 
-
-# 挂载路由
+# 挂路由
 app.include_router(core_router)
